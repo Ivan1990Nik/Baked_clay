5cc17d18f8efbf99112cb174508e293b301f678f
Ivan1990Nik <pelevin.vanyusha90@mail.ru>
2026-01-31 14:59:55 +0300

    add text

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import About from './components/about/About'
-import Contact from './components/contact/Contact'
-import Header from './components/header/Header'
-import Products from './components/products/Products'
-import Reviews from './components/reviews/Reviews'
+import { Routes, Route } from "react-router-dom";
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Products from './components/products/Products';
+import Reviews from './components/reviews/Reviews';
 import Layout from './components/layout/Layout.jsx';
-
-import ProductList from './pages/products/productList'
+import ProductList from './pages/products/productList';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={
-        <>
-          <Layout />
-          <Header />
-          <About />
-          <Products />
-          <Reviews />
-          <Contact />
-        </>
-      } />
-      <Route path="/product" element={<ProductList />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <About />
+            <Products />
+            <Reviews />
+            <Contact />
+          </Layout>
+        }
+      />
+
+      <Route
+        path="/product"
+        element={
+          <Layout>
+            <ProductList />
+          </Layout>
+        }
+      />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
